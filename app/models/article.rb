b923f5cc34aee28b844aa610fdf79d9309e308e6
Peace Pius <peacepius5@gmail.com>
2020-02-17 20:02:39 -0500

    added validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ApplicationRecord
     belongs_to :article
 
+    #Validations
+    validate_presence_of :title, :content
+    
     #Scopes
     scope :active,       -> {where(active: true)}
     scope :alphabetical, -> {order('title')}
